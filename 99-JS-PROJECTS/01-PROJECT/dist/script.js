3238587e6e1a8f491f030219e2485d7190f1ab5b
bhabinpun <poon.bhabin6332@gmail.com>
2024-05-15 16:31:35 +0545

    Background-Color-Changer app. completed

--- 99-JS-PROJECTS/01-PROJECT/dist/script.js
+++ 99-JS-PROJECTS/01-PROJECT/dist/script.js
@@ -1,1 +1,12 @@
-console.log("hello world!")
+console.log("hello world!");
+
+const body = document.querySelector("body");
+
+const color = document.querySelectorAll(".color");
+
+color.forEach(function (value) {
+  value.addEventListener("click", function (event) {
+    body.classList.remove(body.className);
+    body.classList.add(value.classList[1]);
+  });
+});
