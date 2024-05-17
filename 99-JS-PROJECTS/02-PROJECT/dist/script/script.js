e2ada3dd916de1463709325c1b325e8b6178a655
bhabinpun <poon.bhabin6332@gmail.com>
2024-05-17 15:52:12 +0545

    Finalizing 02-js-basic-project

--- 99-JS-PROJECTS/02-PROJECT/dist/script/script.js
+++ 99-JS-PROJECTS/02-PROJECT/dist/script/script.js
@@ -1,1 +1,28 @@
 console.log("hello, world!!");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const weight = parseInt(document.querySelector("#weight").value);
+  const height = parseInt(document.querySelector("#height").value);
+  const result = document.querySelector("#result");
+
+  if (weight < 0 || weight == "" || isNaN(weight)) {
+    result.innerHTML = "Invalid input";
+  } else if (height < 0 || height == "" || isNaN(height)) {
+    result.innerHTML = "Invalid input";
+  } else {
+    const calc = (weight / ((height * height) / 10000)).toFixed(2);
+    var message = "";
+    if (calc < 18.6) {
+      message = "Under-weight";
+    } else if (calc < 24.9 && calc > 18.6) {
+      message = "Normal-weight";
+    } else {
+      message = "Over-weight";
+    }
+    result.innerHTML = `${calc}` + " " + message;
+  }
+});
