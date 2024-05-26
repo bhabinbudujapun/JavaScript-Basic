4072018cae28a7963b84e6fabe75479e956198b8
bhabinpun <poon.bhabin6332@gmail.com>
2024-05-26 17:01:35 +0545

    learning event in js

---
+++ 03-JS-EVENTS/dist/script/script.js
@@ -0,0 +1,14 @@
+// document.getElementById("python").onclick = function () {
+//   alert("alert python");
+// };
+
+// document.getElementById("python-div").addEventListener("click", function (e) {
+//   console.log("PYTHON-DIV");
+// });
+
+// document.getElementById("python-img").addEventListener("click", function (e) {
+//   console.log("PYTHON-IMG");
+//   e.stopPropagation();
+// });
+
+document.getElementsId
