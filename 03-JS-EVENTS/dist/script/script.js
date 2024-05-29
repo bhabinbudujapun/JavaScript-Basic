65bc1f91f50260ceb848d7afa1a0d22d839141f5
bhabinpun <poon.bhabin6332@gmail.com>
2024-05-29 16:23:39 +0545

    implementing or learning js-event manipulation

--- 03-JS-EVENTS/dist/script/script.js
+++ 03-JS-EVENTS/dist/script/script.js
@@ -11,4 +11,35 @@
 //   e.stopPropagation();
 // });
 
-document.getElementsId
+/*
+let imgs = document.querySelectorAll("img");
+
+// SOME OF THE PROPERTIES ACCESS THROUGH 'e'
+imgs.forEach(function (img) {
+  img.addEventListener("click", function (e) {
+    console.log("Clicked element:", e.target);
+    console.log("Event type:", e.type);
+    console.log("Coordinates relative to the viewport:", e.clientX, e.clientY);
+    console.log("Coordinates relative to the document:", e.pageX, e.pageY);
+    console.log("Alt key pressed:", e.altKey);
+  });
+});
+
+//HIDING IMG WHILE CLICKING ON IMG
+imgs.forEach(function (img) {
+  img.addEventListener("click", function (e) {
+    console.log(img.parentElement);
+    console.log(img.parentElement.hidden);
+    console.log((img.parentElement.hidden = true));
+  });
+});
+*/
+
+// HIDING IMG BY CLICKING ON CROSS MARK
+let icons = document.querySelectorAll("i");
+
+icons.forEach(function (icon) {
+  icon.addEventListener("click", function (e) {
+    icon.parentElement.hidden = true;
+  });
+});
